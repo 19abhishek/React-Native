95c27b3226df71f41a186b5456ee0c6e317c8fa1
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-03-22 22:48:38 +0530

    Todo starter

--- App.js
+++ App.js
@@ -1,31 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [text, setText] = useState("First App");
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.btn}>{text}</Text>
-      <Button title="Button" onPress={() => setText("Button Clicked")} />
-      <StatusBar style="auto" />
+    <View style={{ padding: 50, marginTop: 30 }}>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput placeholder="Add Todo" style={styles.textField} />
+        <Button title="Add" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#aaa",
-    alignItems: "center",
-
-    justifyContent: "center",
-  },
-  btn: {
-    borderColor: "red",
-    borderWidth: 2,
-    padding: 4,
-    color: "black",
+  textField: {
+    borderWidth: 1,
   },
 });
